d1eb58e5fe0459675e864d1fedab16c1472aacb4
THEONEMANTEAM <trendranked28@gmail.com>
2026-09-17 11:12:28 +0100

    i learnt sbout scripts code structure alert and variables

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+// This enables that it runs modern js strictly, which is a good practice to avoid errors in your code.
+alert("Hello, World!");
+alert(3
+    + 1
+    + 2);
+// Always put a closing semicolon at the end of a line of code. It is not required, but it is a good practice to avoid errors in your code.
+let message = "Hello!";
+// it defines the variable and assigns the value "Hello!" to it. The variable can be used later in the code.
+alert(message);
+let user = "john",
+    age = 16,
+    message2 = "Broski";
+alert(user);
+alert(age);
+alert(message2);
+// It defines three variables and assigns values to them. The variables can be used later in the code.
