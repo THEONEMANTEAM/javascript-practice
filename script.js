41f7e9a50d191341ba4b0d4c18a491d5f08d2c54
THEONEMANTEAM <trendranked28@gmail.com>
2026-09-18 15:12:45 +0100

    I learnt new syntax like how to use typeof better and also how to convert a value to strings if it in number and how to convert a value in string to number

--- script.js
+++ script.js
@@ -29,3 +29,29 @@ alert(`the result is ${2 + 2}`);
 // you can also use string + variable name alert("let name be" + user).
 let isgreater = 20 > 8
 alert(isgreater);
+// This defines a variable isgreater and assigns the result of the comparison 20 > 8 to it, which is true. It then shows the value of isgreater in an alert box.
+let name = prompt("what is your name?", "");
+alert("Hello, " + name);
+// The prompt function shows a dialog box that asks the user for input. The first argument is the message to display, and the second argument is the default value (which is an empty string in this case). The user's input is then stored in the variable name, and an alert box greets the user with their name.
+alert(`Welcome ${name}`);
+let isKing = confirm("Are you the king?");
+alert(isKing);
+// The confirm function shows a dialog box with OK and Cancel buttons. It returns true if the user clicks OK, and false if the user clicks Cancel. The result is stored in the variable isKing, and an alert box shows the value of isKing.
+let isOnline = true;
+alert(typeof isOnline);
+// it should show boolean, because isOnline is a boolean variable. The typeof operator returns a string that indicates the type of the operand.
+isOnline = String(isOnline);
+alert(typeof isOnline);
+// it should show string, because isOnline has been converted to a string using the String() function. The typeof operator returns a string that indicates the type of the operand.
+alert(isOnline);
+let age2 = 25;
+alert(typeof age2);
+age2 = String(age2);
+alert(typeof age2);
+alert(age2);
+let str = "345 + 1";
+alert(typeof str);
+let num = Number(str);
+alert(typeof num);
+alert(num);
+// it should show number, because str has been converted to a number using the Number() function. The typeof operator returns a string that indicates the type of the operand.
